4b4fcb30dfa1d62481ff54792b5707c13697503a
Jason Katz <jkatz94@gmail.com>
2015-09-20 20:29:34 -0400

    ps2: Finished bitmasking

--- ps2/main.c
+++ ps2/main.c
@@ -10,6 +10,8 @@
 
 void listDir(char * filePath);
 void printData(char * dirPath, struct dirent * de);
+char getType(struct stat sb);
+char * getPermissions(struct stat sb);
 
 int main(int argc, char **argv) {
 
@@ -97,7 +99,110 @@ void printData(char * dirPath, struct dirent * de) {
     strcat(path, de->d_name);
 
     struct stat sb;
-    stat(dirPath, &sb);
-    printf("\t%s\n", de->d_name);
-    //printf("\t%s\n", path);
+    if (lstat(path, &sb) == -1) {
+        printf("stat failed: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    printf("%04X/%d\t", (int)sb.st_dev, (int)sb.st_ino);
+    printf("%c%s\t", getType(sb), getPermissions(sb));
+    printf("%s", de->d_name);
+    printf("\n");
+}
+
+char getType(struct stat sb) {
+    char c;
+
+    switch(sb.st_mode & S_IFMT) {
+    case S_IFREG:
+        c = '-';
+        break;
+    case S_IFDIR:
+        c = 'd';
+        break;
+    case S_IFLNK:
+        c = 'l';
+        break;
+    case S_IFCHR:
+        c = 'c';
+        break;
+    case S_IFBLK:
+        c = 'b';
+        break;
+    case S_IFIFO:
+        c = 'p';
+        break;
+    case S_IFSOCK:
+        c = 's';
+        break;
+    default:
+        // Other cases without macros
+        switch((sb.st_mode & S_IFMT) >> 12) {
+        case 3: // S_IFMPC
+            c = 'm';
+            break;
+        case 5: //S_IFNAM
+            c = 'n';
+            break;
+        case 7: //S_IFMPB
+            c = 'M';
+            break;
+        case 9: //S_IFCMP
+            c = 'C';
+            break;
+        case 11: //S_IFSHAD
+            c = 's';
+            break;
+        case 13: //S_IFDOOR
+            c = 'D';
+            break;
+        case 15: //S_IFWHT
+            c = 'w';
+            break;
+        default:
+            c = 'u';
+            break;
+        }
+        break;
+    }
+
+    return c;
+}
+
+char * getPermissions(struct stat sb) {
+    char * result = malloc(10 * sizeof(char));
+    strcpy(result, "---------");
+
+    int val = sb.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+
+    if (val & 256) {
+        result[0] = 'r';
+    }
+    if (val & 128) {
+        result[1] = 'w';
+    }
+    if (val & 64) {
+        result[2] = 'x';
+    }
+    if (val & 32) {
+        result[3] = 'r';
+    }
+    if (val & 16) {
+        result[4] = 'w';
+    }
+    if (val & 8) {
+        result[5] = 'x';
+    }
+    if (val & 4) {
+        result[6] = 'r';
+    }
+    if (val & 2) {
+        result[7] = 'w';
+    }
+    if (val & 1) {
+        result[8] = 'x';
+    }
+
+
+    return result;
 }
