96396d2281d9e4710c443fbb9a8c9c2183018590
Jason Katz <jkatz94@gmail.com>
2015-09-30 15:08:29 -0400

    ps3: Started messing around with exec; removed .out files

--- ps3/main.c
+++ ps3/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char ** argv, char ** envp) {
 
+    char * args[] = { "", "-l" };
 
+    if (execvp("ls", args) == -1) {
+        printf("Could not run execve: %s\n", strerror(errno));
+    }
 
     return 0;
 }
