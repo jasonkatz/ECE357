a49850b0d9d2e4d49b05e5f53eb72a3691b88beb
Jason Katz <jkatz94@gmail.com>
2015-10-11 21:12:06 -0400

    ps4: Pipeline works for one file at a time

--- ps4/main.c
+++ ps4/main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char ** argv) {
 
-    char * commands[2] = { "grep", "more" };
+    char * commands[2] = { "grep", "less" };
 
     int i;
     int bufferSize = 4096; // Default buffer size 4kb
@@ -36,36 +36,47 @@ int main(int argc, char ** argv) {
 
     // Loop through input files
     for (i = 0; i < numInputFiles; ++i) {
-        // Create pipes
+        // Create pipe 1
         int pipe1fds[2], pipe2fds[2];
         if (pipe(pipe1fds) < 0) {
             fprintf(stderr, "Can't create pipe 1 for input file %s: %s\n", inputFiles[i], strerror(errno));
             exit(1);
         }
+
+        // Create pipe 2
         if (pipe(pipe2fds) < 0) {
             fprintf(stderr, "Can't create pipe 2 for input file %s: %s\n", inputFiles[i], strerror(errno));
             exit(1);
         }
 
-        int outfd = pipe1fds[0];
-
-        // Fork process to exec grep and more
+        // Fork process to exec grep
         pid_t pid1;
         int status1;
         struct rusage ru1;
         switch(pid1 = fork()) {
             case 0: {
-                // Dup grep stdin to write end of pipe1
-                if (dup2(pipe1fds[1], STDIN_FILENO) < 0) {
-                    fprintf(stderr, "Can't dup2 stdin of grep to write end of pipe 1: %s\n", strerror(errno));
+                // Close dangling file descriptors
+                close(pipe1fds[1]);
+                close(pipe2fds[0]);
+
+                // Dup grep stdin to read end of pipe 1
+                if (dup2(pipe1fds[0], STDIN_FILENO) < 0) {
+                    fprintf(stderr, "Can't dup2 stdin of grep to read end of pipe 1: %s\n", strerror(errno));
                     exit(1);
                 }
 
-                // Close dangling file descriptor
-                close(pipe1fds[1]);
+                // Dup grep stdout to write end of pipe 2
+                if (dup2(pipe2fds[1], STDOUT_FILENO) < 0) {
+                    fprintf(stderr, "Can't dup2 stdout of grep to write end of pipe 2: %s\n", strerror(errno));
+                    exit(1);
+                }
+
+                // Close dangling file descriptors
+                close(pipe1fds[0]);
+                close(pipe2fds[1]);
 
                 // Exec the child grep process
-                char * args[2] = { commands[0], argv[1] };
+                char * args[3] = { commands[0], argv[1], 0 };
                 execvp(commands[0], args);
                 break;
             }
@@ -75,24 +86,27 @@ int main(int argc, char ** argv) {
                 break;
             }
             default: {
+                // Close dangling file descriptors
+                close(pipe1fds[0]);
+
                 // Open file
                 int infd;
                 if ((infd = open(inputFiles[i], O_RDONLY, 0777)) < 0) {
                     fprintf(stderr, "File open error (%s): %s\n", inputFiles[i], strerror(errno));
                     exit(1);
                 }
-                // Read from each file
+                // Read from the file
                 int byteNum;
                 char buf[bufferSize];
                 while ((byteNum = read(infd, buf, bufferSize)) > 0) {
-                    int wrstatus = write(outfd, buf, byteNum);
+                    int wrstatus = write(pipe1fds[1], buf, byteNum);
                     if (wrstatus <= 0) {
-                        fprintf(stderr, "Write error: %s\n", strerror(errno));
+                        fprintf(stderr, "Write error on fd %d: %s\n", pipe1fds[1], strerror(errno));
                         exit(1);
                     } else if (wrstatus < byteNum) {
                         // Write to stdout and retry write on partial write
                         printf("Partial write from buffer to stdout\n");
-                        write(outfd, buf, byteNum - wrstatus);
+                        write(pipe1fds[1], buf, byteNum - wrstatus);
                     }
                 }
 
@@ -101,14 +115,50 @@ int main(int argc, char ** argv) {
                     exit(1);
                 }
 
-                // Close file
-                int closeStatus = close(outfd);
-
-                if (closeStatus == -1) {
-                    fprintf(stderr, "File close error: %s\n", strerror(errno));
-                }
+                // Close output file
+                close(pipe1fds[1]);
 
                 if (wait3(&status1, 0, &ru1) < 0) {
+                    fprintf(stderr, "Wait3 error: %s\n", strerror(errno));
+                    exit(1);
+                }
+                break;
+            }
+        }
+
+        // Fork process to exec more
+        pid_t pid2;
+        int status2;
+        struct rusage ru2;
+        switch(pid2 = fork()) {
+            case 0: {
+                // Close dangling file descriptor
+                close(pipe2fds[1]);
+
+                // Dup grep stdin to read end of pipe 2
+                if (dup2(pipe2fds[0], STDIN_FILENO) < 0) {
+                    fprintf(stderr, "Can't dup2 stdin of more to read end of pipe 2: %s\n", strerror(errno));
+                    exit(1);
+                }
+
+                // Close dangling file descriptor
+                close(pipe1fds[0]);
+
+                // Exec the child grep process
+                char * args[3] = { commands[1], 0 };
+                execvp(commands[1], args);
+                break;
+            }
+            case -1: {
+                fprintf(stderr, "Fork failed: %s\n", strerror(errno));
+                exit(1);
+                break;
+            }
+            default: {
+                close(pipe2fds[0]);
+                close(pipe2fds[1]);
+
+                if (wait3(&status2, 0, &ru2) < 0) {
                     fprintf(stderr, "Wait3 error: %s\n", strerror(errno));
                     exit(1);
                 }
