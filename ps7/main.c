62b9c113b0be9db1a213c808dca6387534ae34f8
Jason Katz <jkatz94@gmail.com>
2015-11-09 12:19:56 -0500

    ps7: Retesting on pi

--- ps7/main.c
+++ ps7/main.c
@@ -18,8 +18,13 @@ int tas(volatile char *lock);
 int main() {
 
     int fd;
-    if ((fd = open("testfile.txt", O_RDWR | O_CREAT | O_TRUNC, 0777) < 0)) {
+    if ((fd = open("testfile.txt", O_RDWR | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE) < 0)) {
         fprintf(stderr, "Couldn't open testfile.txt: %s\n", strerror(errno));
+        exit(1);
+    }
+    char * buf = "some text";
+    if (write(fd, buf, 10) < 0) {
+        fprintf(stderr, "Couldn't write to testfile.txt: %s\n", strerror(errno));
         exit(1);
     }
     struct stat sb;
@@ -28,10 +33,33 @@ int main() {
         exit(1);
     }
 
-    char * map;
-    if ((map = mmap(0, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0) < 0)) {
+    int * map;
+    if ((map = (int *)mmap(0, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0) < 0)) {
         fprintf(stderr, "Couldn't map to file testfile.txt: %s\n", strerror(errno));
+        close(fd);
         exit(1);
+    }
+
+    map[0] = 0;
+
+    int i;
+    for (i = 0; i < N_PROC; ++i) {
+        pid_t pid;
+        switch(pid = fork()) {
+            case 0:
+                break;
+            case -1:
+                fprintf(stderr, "Couldn't fork process: %s\n", strerror(errno));
+                exit(1);
+                break;
+            default: {
+                int j;
+                for (j = 0; j < 1000; ++j) {
+                    map[0] += 1;
+                }
+                break;
+            }
+        }
     }
 
     if (close(fd) < 0) {
